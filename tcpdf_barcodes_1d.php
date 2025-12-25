b004f506c56100f9a9978af2267f5726196bc353
lufeiservices <he.bo@seagm.com>
2025-12-25 18:33:14 +0800

    update

--- tcpdf_barcodes_1d.php
+++ tcpdf_barcodes_1d.php
@@ -9,32 +9,32 @@
 // -------------------------------------------------------------------
 // Copyright (C) 2008-2014 Nicola Asuni - Tecnick.com LTD
 //
-// This file is part of TCPDF software library.
+// This file is part of tcpdf software library.
 //
-// TCPDF is free software: you can redistribute it and/or modify it
+// tcpdf is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// TCPDF is distributed in the hope that it will be useful, but
+// tcpdf is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with TCPDF.  If not, see <http://www.gnu.org/licenses/>.
+// along with tcpdf.  If not, see <http://www.gnu.org/licenses/>.
 //
 // See LICENSE.TXT file for more information.
 // -------------------------------------------------------------------
 //
 // Description : PHP class to creates array representations for
-//               common 1D barcodes to be used with TCPDF.
+//               common 1D barcodes to be used with tcpdf.
 //
 //============================================================+
 
 /**
  * @file
- * PHP class to creates array representations for common 1D barcodes to be used with TCPDF.
+ * PHP class to creates array representations for common 1D barcodes to be used with tcpdf.
  * @package com.tecnick.tcpdf
  * @author Nicola Asuni
  * @version 1.0.027
@@ -42,7 +42,7 @@
 
 /**
  * @class TCPDFBarcode
- * PHP class to creates array representations for common 1D barcodes to be used with TCPDF (http://www.tcpdf.org).<br>
+ * PHP class to creates array representations for common 1D barcodes to be used with tcpdf (http://www.tcpdf.org).<br>
  * @package com.tecnick.tcpdf
  * @version 1.0.027
  * @author Nicola Asuni
--- tcpdf_barcodes_2d.php
+++ tcpdf_barcodes_2d.php
@@ -9,32 +9,32 @@
 // -------------------------------------------------------------------
 // Copyright (C) 2009-2014 Nicola Asuni - Tecnick.com LTD
 //
-// This file is part of TCPDF software library.
+// This file is part of tcpdf software library.
 //
-// TCPDF is free software: you can redistribute it and/or modify it
+// tcpdf is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// TCPDF is distributed in the hope that it will be useful, but
+// tcpdf is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with TCPDF.  If not, see <http://www.gnu.org/licenses/>.
+// along with tcpdf.  If not, see <http://www.gnu.org/licenses/>.
 //
 // See LICENSE.TXT file for more information.
 // -------------------------------------------------------------------
 //
 // Description : PHP class to creates array representations for
-//               2D barcodes to be used with TCPDF.
+//               2D barcodes to be used with tcpdf.
 //
 //============================================================+
 
 /**
  * @file
- * PHP class to creates array representations for 2D barcodes to be used with TCPDF.
+ * PHP class to creates array representations for 2D barcodes to be used with tcpdf.
  * @package com.tecnick.tcpdf
  * @author Nicola Asuni
  * @version 1.0.015
@@ -42,7 +42,7 @@
 
 /**
  * @class TCPDF2DBarcode
- * PHP class to creates array representations for 2D barcodes to be used with TCPDF (http://www.tcpdf.org).
+ * PHP class to creates array representations for 2D barcodes to be used with tcpdf (http://www.tcpdf.org).
  * @package com.tecnick.tcpdf
  * @version 1.0.015
  * @author Nicola Asuni
