d331491645433ab73d6e77a84e366c93def766e2
lufeiservices <he.bo@seagm.com>
2025-12-25 18:22:45 +0800

    update

--- include/barcodes/pdf417.php
+++ include/barcodes/pdf417.php
@@ -9,27 +9,27 @@
 // -------------------------------------------------------------------
 // Copyright (C) 2010-2013  Nicola Asuni - Tecnick.com LTD
 //
-// This file is part of TCPDF software library.
+// This file is part of tcpdf software library.
 //
-// TCPDF is free software: you can redistribute it and/or modify it
+// tcpdf is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// TCPDF is distributed in the hope that it will be useful, but
+// tcpdf is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with TCPDF.  If not, see <http://www.gnu.org/licenses/>.
+// along with tcpdf.  If not, see <http://www.gnu.org/licenses/>.
 //
 // See LICENSE.TXT file for more information.
 // -------------------------------------------------------------------
 //
 // DESCRIPTION :
 //
-// Class to create PDF417 barcode arrays for TCPDF class.
+// Class to create PDF417 barcode arrays for tcpdf class.
 // PDF417 (ISO/IEC 15438:2006) is a 2-dimensional stacked bar code created by Symbol Technologies in 1991.
 // It is one of the most popular 2D codes because of its ability to be read with slightly modified handheld laser or linear CCD scanners.
 // TECHNICAL DATA / FEATURES OF PDF417:
@@ -45,7 +45,7 @@
 
 /**
  * @file
- * Class to create PDF417 barcode arrays for TCPDF class.
+ * Class to create PDF417 barcode arrays for tcpdf class.
  * PDF417 (ISO/IEC 15438:2006) is a 2-dimensional stacked bar code created by Symbol Technologies in 1991.
  * (requires PHP bcmath extension)
  * @package com.tecnick.tcpdf
@@ -84,7 +84,7 @@ if (!defined('PDF417DEFS')) {
 
 /**
  * @class PDF417
- * Class to create PDF417 barcode arrays for TCPDF class.
+ * Class to create PDF417 barcode arrays for tcpdf class.
  * PDF417 (ISO/IEC 15438:2006) is a 2-dimensional stacked bar code created by Symbol Technologies in 1991.
  * @package com.tecnick.tcpdf
  * @author Nicola Asuni
@@ -93,7 +93,7 @@ if (!defined('PDF417DEFS')) {
 class PDF417 {
 
 	/**
-	 * Barcode array to be returned which is readable by TCPDF.
+	 * Barcode array to be returned which is readable by tcpdf.
 	 * @protected
 	 */
 	protected $barcode_array = array();
@@ -724,8 +724,8 @@ class PDF417 {
 	}
 
 	/**
-	 * Returns a barcode array which is readable by TCPDF
-	 * @return array barcode array readable by TCPDF;
+	 * Returns a barcode array which is readable by tcpdf
+	 * @return array barcode array readable by tcpdf;
 	 * @public
 	 */
 	public function getBarcodeArray() {
